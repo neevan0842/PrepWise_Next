be299c777de72f2faa8da95d83c2523490e300ce
Naveen Manoj <manojnaveen085@gmail.com>
2025-03-31 12:26:36 +0530

    env variables changed

--- src/firebase/client.ts
+++ src/firebase/client.ts
@@ -4,13 +4,13 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCGg9PsLuL1PIv06FaqGPKtdlLwwyZgt5A",
-  authDomain: "prepwise-ed3ae.firebaseapp.com",
-  projectId: "prepwise-ed3ae",
-  storageBucket: "prepwise-ed3ae.firebasestorage.app",
-  messagingSenderId: "959291184727",
-  appId: "1:959291184727:web:31c8a217faf5551a67b512",
-  measurementId: "G-XFTNTTCXPP",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
